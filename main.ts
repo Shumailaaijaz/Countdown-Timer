2b3e73eea24d9b0aab9f211c93ec3bc5a0644121
Shumailaaijaz <159613829+Shumailaaijaz@users.noreply.github.com>
2024-06-12 04:37:31 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import {differenceInSeconds} from "date-fns";
+
+
+let res = await inquirer.prompt([
+    {
+        name : 'userInput',
+        type : 'number',
+        message : 'Enter seconds in number',
+        validate : (input)=>{
+            if(isNaN(input)){
+                return 'Enter a valid number'
+            }else if(input > 60){
+                return 'Enter a number under 60'
+            }else{
+                return true;
+            }
+        }
+    }
+]);
+
+let input = res.userInput;
+
+function startTime(val:number){
+    const initialTime = new Date().setSeconds(new Date().getSeconds()+val);
+    const intervelTime = new Date(initialTime);
+    setInterval((()=>{
+        const currentTime = new Date()
+        const TimeDiff = differenceInSeconds(intervelTime,currentTime);
+        
+        if(TimeDiff <= 0){
+            console.log(`Time is over`);
+            process.exit()
+        }
+        let minuts = Math.floor((TimeDiff%(3600*24))/3600)
+        let secondz = Math.floor(TimeDiff%60)
+        console.log(chalk.blueBright.italic.bold(`${minuts.toString().padStart(2,'0')}: ${secondz.toString().padStart(2,'0')}`));
+        
+    }),1000)
+}
+startTime(input)
